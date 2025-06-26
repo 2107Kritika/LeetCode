d2946f085bc7567aa6c7896ac41aa85f7e9ee584
Kritika Rana <123441380+2107Kritika@users.noreply.github.com>
2025-06-26 12:29:18 +0530

    Time: 1 ms (87.12%), Space: 41.1 MB (31.31%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        double n=Math.sqrt(x);
-        return (int)Math.floor(n);
+        if(x==0 || x==1)
+        return x;
+
+        int l=1;
+        int h=x;
+        int m;
+
+        while(l<=h){
+            m=l+(h-l)/2;
+
+            if((long)m * m>x)
+            h=m-1;
+            else if((long)m*m==x)
+            return m;
+            else
+            l=m+1;
+        }
+        return Math.round(h);
     }
 }
