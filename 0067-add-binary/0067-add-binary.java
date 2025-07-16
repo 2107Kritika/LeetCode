02e378ef3cdc631c754fc23c392bc2e0ab0bd407
Kritika Rana <123441380+2107Kritika@users.noreply.github.com>
2025-07-16 10:35:48 +0530

    Time: 9 ms (7.42%), Space: 43.7 MB (10.39%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger x = new BigInteger(a, 2);
+        BigInteger y = new BigInteger(b, 2);
+
+        while (!y.equals(BigInteger.ZERO)) {
+            BigInteger answer = x.xor(y);
+            BigInteger carry = x.and(y).shiftLeft(1);
+
+            x = answer;
+            y = carry;
+        }
+
+        return x.toString(2);
+    }
+}
